01de650b839fc525fb34f42d382d39beedd72e00
Jon Rimmer <jon.rimmer@gmail.com>
2018-12-13 20:16:25 +0000

    Updates

---
+++ precache-manifest.3b80df91a751a62e2dbea092f40c1ab5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "107e593fc7dceaba885e",
+    "url": "/guitar-quiz/static/css/main.c1681eb0.chunk.css"
+  },
+  {
+    "revision": "107e593fc7dceaba885e",
+    "url": "/guitar-quiz/static/js/main.107e593f.chunk.js"
+  },
+  {
+    "revision": "c6848162ae3eb25e010e",
+    "url": "/guitar-quiz/static/js/1.c6848162.chunk.js"
+  },
+  {
+    "revision": "9321c1a2e7835eafa978",
+    "url": "/guitar-quiz/static/js/runtime~main.9321c1a2.js"
+  },
+  {
+    "revision": "eceedf03da569cbe20e2865e7becbbca",
+    "url": "/guitar-quiz/index.html"
+  }
+];
