a9c6a3ca159fe3eeab0545057441fa0bb4abaa93
Jon Rimmer <jon.rimmer@gmail.com>
2019-01-14 16:48:34 +0000

    Updates

---
+++ precache-manifest.b33d22f3d7663679b94fc3373c5c6c2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0dbe9bf19af9969294d",
+    "url": "/guitar-quiz/static/css/main.b3a19abe.chunk.css"
+  },
+  {
+    "revision": "d0dbe9bf19af9969294d",
+    "url": "/guitar-quiz/static/js/main.d0dbe9bf.chunk.js"
+  },
+  {
+    "revision": "8d7f1d7f7bfc8ce6382d",
+    "url": "/guitar-quiz/static/js/1.8d7f1d7f.chunk.js"
+  },
+  {
+    "revision": "9321c1a2e7835eafa978",
+    "url": "/guitar-quiz/static/js/runtime~main.9321c1a2.js"
+  },
+  {
+    "revision": "ad1e9ac65af887b46634e951f7663c2b",
+    "url": "/guitar-quiz/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/guitar-quiz/precache-manifest.36fe36ba440071e500b75d83234b93ab.js"
+  "/guitar-quiz/precache-manifest.b33d22f3d7663679b94fc3373c5c6c2b.js"
 );
 
 workbox.clientsClaim();
