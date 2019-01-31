abda01abccee76859bb0e82aaa0624862cb5e576
Jon Rimmer <jon.rimmer@gmail.com>
2019-01-31 12:30:32 +0000

    Updates

---
+++ precache-manifest.a4783cacefbf5f3f8ad5d6fbfc46d115.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74f6df96038939990ed3",
+    "url": "/fretfull/static/css/main.77532603.chunk.css"
+  },
+  {
+    "revision": "74f6df96038939990ed3",
+    "url": "/fretfull/static/js/main.74f6df96.chunk.js"
+  },
+  {
+    "revision": "96fc0ef12e111ca340ba",
+    "url": "/fretfull/static/js/1.96fc0ef1.chunk.js"
+  },
+  {
+    "revision": "82d1c2fa300d890ff78a",
+    "url": "/fretfull/static/js/runtime~main.82d1c2fa.js"
+  },
+  {
+    "revision": "fddc828f7516ec98d6867191d23e7781",
+    "url": "/fretfull/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/fretfull/precache-manifest.6aeed48fb14e72197d61dd3581f370fe.js"
+  "/fretfull/precache-manifest.a4783cacefbf5f3f8ad5d6fbfc46d115.js"
 );
 
 workbox.clientsClaim();
