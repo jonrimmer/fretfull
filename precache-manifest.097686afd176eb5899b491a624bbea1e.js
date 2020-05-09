f91a2e3799e5b2f0465340d8c96df7921daf4ca6
Jon Rimmer <jon.rimmer@gmail.com>
2020-05-09 11:10:34 +0100

    Updates

---
+++ precache-manifest.097686afd176eb5899b491a624bbea1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f18a645f78ab77c161b3c7297329ce5",
+    "url": "/fretfull/index.html"
+  },
+  {
+    "revision": "bc9986d90b799a111869",
+    "url": "/fretfull/static/css/main.22465dd2.chunk.css"
+  },
+  {
+    "revision": "ef0ecfd86e075623b7b2",
+    "url": "/fretfull/static/js/2.88156c82.chunk.js"
+  },
+  {
+    "revision": "c27e167d6e2cc1d9eddf7fde2be56a6c",
+    "url": "/fretfull/static/js/2.88156c82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc9986d90b799a111869",
+    "url": "/fretfull/static/js/main.a13efaf1.chunk.js"
+  },
+  {
+    "revision": "2e84230bab031b2c2d5f",
+    "url": "/fretfull/static/js/runtime-main.cbb40034.js"
+  }
+]);
